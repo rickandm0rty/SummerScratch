340f94bff40115c95b03b2c4449f2284d17e9aef
Kyle Nelson <kylenelson272@u.boisestate.edu>
2018-05-13 23:21:28 -0600

    Basic Powershell Scratch Files and Q's for Work
    ^

---
+++ Temp/ScratchFile.ps1
@@ -0,0 +1,17 @@
+﻿$Student1 = 74
+$Student2 = 85
+$Student3 = 77
+
+$possibleScore = 100
+
+$global;$cumlative = $Student1 + $Student2 + $Student3
+Write-Host $cumlative
+
+$ans = $cumlative / ($possibleScore * 3) * 100
+
+Write-Host $ans
+
+
+
+
+
